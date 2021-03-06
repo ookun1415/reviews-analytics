012e4cd4b949bd269dd96660a20165bbfbf8e3c9
ookun <jeffery12369874@gmail.com>
2021-03-07 00:02:02 +0800

    add filtering fuction

--- read.py
+++ read.py
@@ -12,3 +12,10 @@ for d in data:
 	x = len(d)
 	sum_len = sum_len + x
 print('留言平均長度為:', sum_len/1000000)
+new = []
+for d in data:
+	if len(d) > 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度超過100')
+print(new[0])
+print(new[1])
